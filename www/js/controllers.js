7c58a00a1a7c32e84cecd835aa4fce080032215b
bellviciouz <bell.viciouz@gmail.com>
2015-10-05 21:42:43 +0000

    sql funciona

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,13 +23,13 @@ angular.module('starter.controllers', ['ionic','ngCordova'])
      });
      
      */
-   //      if(window.cordova) {
+         if(window.cordova) {
       // App syntax
       db = $cordovaSQLite.openDB("agenda.db");
-//    } else {
+    } else {
       // Ionic serve syntax
-  //    db = window.openDatabase("agenda.db", "1", "My app", -1);
- //   }
+      db = window.openDatabase("agenda.db", "1", "My app", -1);
+    }
 $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS personas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(255), apellido varchar(255), telefono varchar(255), email varchar(255))');
   
         $cordovaSQLite.execute(db, 'INSERT INTO personas (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
@@ -61,7 +61,7 @@ $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS personas (id INTEGER PRIM
    
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats,$ionicPopup,$cordovaSQLite) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -69,11 +69,33 @@ $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS personas (id INTEGER PRIM
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+     if(window.cordova) {
+      // App syntax
+      db = $cordovaSQLite.openDB("agenda.db");
+    } else {
+      // Ionic serve syntax
+      db = window.openDatabase("agenda.db", "1", "My app", -1);
+    }
+$cordovaSQLite.execute(db, 'SELECT * FROM personas ORDER BY id DESC')
+            .then(
+                function(result) {
+ 
+                    if (result.rows.length > 0) {
+ 			for(var i=0;i<result.rows.length;i++)
+			{
+			
+			$scope.chats = result.rows.item(i).apellido;
+                        }
+                    }
+                },
+                function(error) {
+                    $scope.statusMessage = "Error on loading: " + error.message;
+                }
+            );
+  //$scope.chats = Chats.all();
+  //$scope.remove = function(chat) {
+//    Chats.remove(chat);
+ // };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
